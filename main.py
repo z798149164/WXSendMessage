d2bf57064c034c25ba39c9a231f5551a03e9c1fa
LiCyan <80187311+z798149164@users.noreply.github.com>
2022-08-23 17:08:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
+print(start_date)
 
 def get_weather():
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
